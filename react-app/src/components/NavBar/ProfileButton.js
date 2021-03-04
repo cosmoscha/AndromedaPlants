9d6d1cac2d6bf88f7ffb4117b85b2dfbd4a1ffa1
River <cosmos.cha1997@gmail.com>
2021-03-04 01:20:47 -0500

    working on dropdown css issues

--- react-app/src/components/NavBar/ProfileButton.js
+++ react-app/src/components/NavBar/ProfileButton.js
@@ -1,7 +1,46 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "../auth/LogoutButton";
+import "./NavBar.css";
 
-const ProfileButton = () => {
-  return <div>hello world</div>;
+const ProfileButton = ({ setAuthenticated }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  let profileButtons = (
+    <div className="menuButtons">
+      <div>
+        <NavLink to="/login" exact={true} activeClassName="active">
+          Login
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/sign-up" exact={true} activeClassName="active">
+          Sign Up
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/users" exact={true} activeClassName="active">
+          Users
+        </NavLink>
+      </div>
+      <div>
+        <LogoutButton setAuthenticated={setAuthenticated} />
+      </div>
+    </div>
+  );
+  return (
+    <>
+      <div className="button-container">
+        <button
+          onClick={() => setShowMenu(showMenu === true ? false : true)}
+          id="dropdown_button"
+        >
+          button
+        </button>
+        {showMenu && profileButtons}
+      </div>
+    </>
+  );
 };
 
 export default ProfileButton;
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,37 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import LogoutButton from "../auth/LogoutButton";
 import "./NavBar.css";
 import ProfileButton from "./ProfileButton";
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav>
       <div className="menu">
-        <ProfileButton />
         <div>
           <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </div>
-        <div className="menuButtons">
-          <div>
-            <NavLink to="/login" exact={true} activeClassName="active">
-              Login
-            </NavLink>
-          </div>
-          <div>
-            <NavLink to="/sign-up" exact={true} activeClassName="active">
-              Sign Up
-            </NavLink>
-          </div>
-          <div>
-            <NavLink to="/users" exact={true} activeClassName="active">
-              Users
-            </NavLink>
-          </div>
-          <div>
-            <LogoutButton setAuthenticated={setAuthenticated} />
-          </div>
+        <div>
+          <ProfileButton setAuthenticated={setAuthenticated} />
         </div>
       </div>
     </nav>
