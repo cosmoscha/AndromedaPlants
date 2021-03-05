df85cf912d4edf4dc0f280fd80bfec1b1717fa71
River <cosmos.cha1997@gmail.com>
2021-03-04 20:07:13 -0500

    seeders

--- app/seeds/user_products.py
+++ app/seeds/user_products.py
@@ -2,6 +2,7 @@ from app.models import db, UserProduct
 
 
 def seed_userProducts():
+
 def undo_userProducts():
     db.session.execute('TRUNCATE userProducts CASCADE;')
     db.session.execute("ALTER SEQUENCE userProducts_id_seq RESTART WITH 1")
